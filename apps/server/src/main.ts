9fff233317406d42b41cdc52090104e90b1bf7e1
Iossif <iossif123gg@gmail.com>
2023-06-15 11:58:47 +0300

    add more basic db operations

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import * as path from 'path';
 import mockBlocks from './mockBlocks';
 import { connectDb } from './db';
+import { ObjectId } from 'mongodb';
 
 const app = express();
 
@@ -11,12 +12,18 @@ app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.get('/', async (_req, res) => {
   const db = await connectDb();
 
-  const result = await db.collection('code-versions').insertOne({
-    name: 'testing',
-  });
+  // Find all snippets
+  const snippets = await db.collection('snippets').find().toArray();
 
-  return res.json({ result: result.insertedId });
+  // For each snippet, count the number of versions
+  const snippetsWithVersionCount = await Promise.all(snippets.map(async snippet => {
+    const versionCount = await db.collection('versions').countDocuments({ snippetId: snippet._id });
+    return { ...snippet, versionCount };
+  }));
+
+  return res.json(snippetsWithVersionCount);
 });
+
 
 app.post('/snippets', async (req, res) => {
   const db = await connectDb();
@@ -39,6 +46,56 @@ app.post('/snippets', async (req, res) => {
 
   // Return the name of the created snippet
   return res.status(200).json({ name });
+});
+
+app.put('/snippets/:id/add-version', async (req, res) => {
+  const db = await connectDb();
+  const { id } = req.params;
+  const { version, status, code, explanation } = req.body;
+
+  // Validate provided data
+  if (!version || !status || !code || !explanation) {
+    return res.status(400).send('Incomplete version data provided');
+  }
+
+  // Validate the provided snippet ID
+  let snippet;
+  try {
+    snippet = await db
+      .collection('snippets')
+      .findOne({ _id: new ObjectId(id) });
+  } catch (error) {
+    return res.status(400).send('Invalid snippet ID provided');
+  }
+
+  // If the snippet doesn't exist, return a 404 error
+  if (!snippet) {
+    return res.status(404).send('Snippet not found');
+  }
+
+  // Check if the version already exists
+  const existingVersion = await db
+    .collection('versions')
+    .findOne({ snippetId: new ObjectId(id), version });
+
+  if (existingVersion) {
+    return res.status(409).send('This version already exists for the snippet');
+  }
+
+  // Add the new version
+  const newVersion = {
+    snippetId: new ObjectId(id),
+    version,
+    status,
+    code,
+    explanation,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  const result = await db.collection('versions').insertOne(newVersion);
+
+  return res.status(200).json({ versionId: result.insertedId });
 });
 
 app.get('/api', (req, res) => {
