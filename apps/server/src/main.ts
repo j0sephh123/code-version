e6d3d206dea3b7c60bb94ed4487f4400fef3f7b6
Iossif <iossif123gg@gmail.com>
2023-06-15 11:51:09 +0300

    api - basic functionality for creating a snippet

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,6 +5,7 @@ import { connectDb } from './db';
 
 const app = express();
 
+app.use(express.json());
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', async (_req, res) => {
@@ -15,6 +16,29 @@ app.get('/', async (_req, res) => {
   });
 
   return res.json({ result: result.insertedId });
+});
+
+app.post('/snippets', async (req, res) => {
+  const db = await connectDb();
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).send('name not provided');
+  }
+
+  // Check if the name already exists in the database
+  const existingSnippet = await db.collection('snippets').findOne({ name });
+
+  if (existingSnippet) {
+    // If the name exists, return a 404 error
+    return res.status(404).send('A snippet with this name already exists');
+  }
+
+  // If the name doesn't exist, insert the new snippet
+  await db.collection('snippets').insertOne({ name });
+
+  // Return the name of the created snippet
+  return res.status(200).json({ name });
 });
 
 app.get('/api', (req, res) => {
